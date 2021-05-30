3ad3514c437f87946043586a158e3c3d99acebe2
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2021-05-30 22:45:52 +0530

    Intro to analysing players, dumping data-frame to csv file

--- Prediction/a.py
+++ Prediction/a.py
@@ -20,4 +20,45 @@ indian_forwards = deepcopy(isl_df[(isl_df['country_id'] == 1) & (isl_df['positio
 indian_forwards.reset_index(drop = True, inplace = True)
 # print(indian_forwards)
 
-print(indian_forwards[indian_forwards['name'].str.contains('Singh')])
+# print(indian_forwards[indian_forwards['name'].str.contains('Singh')])
+
+
+#####  Level the Playing Field  #####
+
+fwd_min_df = indian_forwards.groupby(['id', 'name'])['minutes_played'].sum().reset_index()
+# print(fwd_min_df)
+
+fig = px.bar(x = 'name', y = 'minutes_played', data_frame = fwd_min_df)
+# fig.show()
+
+# print(fwd_min_df[['name', 'minutes_played']])
+
+# print(fwd_min_df['name'].to_list())
+# print(fwd_min_df['minutes_played'].to_list())
+
+# x_axis = fwd_min_df['name'].to_list()
+# y_axis = fwd_min_df['minutes_played'].to_list()
+
+# plt.bar(x_axis, y_axis)
+# plt.xticks(x_axis, rotation = 30)
+# plt.show(block=True)
+
+
+
+#####  Calculating per 90 stats  #####
+
+indian_forwards = indian_forwards[indian_forwards['minutes_played'] > 0] # eliminate stats of players who played <= 0 minutes
+
+# create a list of columns whose 'per 90' stats are to be calculated
+cols_for_per90 = ['events.goals', 'events.assists', 'events.shots', 'events.shots_on_target', 'events.chances_created', 'events.fouls_suffered', 'touches.total', 'touches.aerial_duel.won', 'touches.ground_duel.won']
+
+per90_cols = [i + "_per90" for i in cols_for_per90] # creating list of per 90 cols
+
+# creating per90 columns :-
+for k in range(0, len(per90_cols)):
+    indian_forwards[per90_cols[k]] = indian_forwards[cols_for_per90[k]].divide(indian_forwards['minutes_played']).multiply(90)
+
+per90_df = indian_forwards[per90_cols]
+# print(per90_df)
+
+per90_df.to_csv('per90.csv', sep = '\t') # dumping the data frame into a csv file
